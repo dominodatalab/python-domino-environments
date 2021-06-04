14ad33605e1004f86a75414b9fadd82f67816952
Caidan Williams <caidanstevenwilliams@gmail.com>
2021-06-04 16:54:13 -0400

    Add create_environment func and EnvironmentManager class

--- domino_environments/__init__.py
+++ domino_environments/__init__.py
@@ -1,7 +1,9 @@
-from ._environments import Environment, ImageType, ClusterType
+from ._environments import Environment, EnvironmentManager, ImageType, ClusterType, Visibility
 
 __all__ = [
     "Environment",
+    "EnvironmentManager",
     "ImageType",
     "ClusterType",
+    "Visibility",
 ]
--- domino_environments/_environments.py
+++ domino_environments/_environments.py
@@ -29,6 +29,12 @@ class ClusterType:
     spark = "Spark"
 
 
+class Visibility:
+    global_ = "Global"
+    organization = "Organization"
+    private = "Private"
+
+
 class _EnvironmentRoutes:
     def __init__(self, host: str):
         self.host = host
@@ -42,6 +48,9 @@ class _EnvironmentRoutes:
     def environment_default_get(self):
         return self._build_environments_url() + "/defaultEnvironment"
 
+    def environment_create(self):
+        return self.host + "/environments"
+
     def environment_get(self, environment_id):
         return self._build_environments_url() + f"/{environment_id}"
 
@@ -52,16 +61,54 @@ class _EnvironmentRoutes:
         return self.host + f"/environments/{environment_id}/revisions"
 
     def revision_download(self, environment_id, revision_id):
-        return self.host + f"/v1/environments/{environment_id}/revisions/{revision_id}/dockerImageSourceProjectWeb"
+        return (
+            self.host
+            + f"/v1/environments/{environment_id}/revisions/{revision_id}/dockerImageSourceProjectWeb"
+        )
 
 
 class Environment:
-    _env_data = dict()
-    _env_details = dict()
-    _default_env_data = dict()
-    _default_env_details = dict()
+    def __init__(self, data: dict):
+        self._data = data
 
-    def __init__(self, environment_id: str, base_url: str, api_key=None, domino_token_file=None):
+    @property
+    def id(self) -> str:
+        return self._data.get("id")
+
+    @property
+    def latest_revision(self) -> dict:
+        return self._data.get("latestRevision")
+
+    @property
+    def active_revision(self) -> dict:
+        return self._data.get("selectedRevision")
+
+    @property
+    def archived(self) -> bool:
+        return self._data.get("archived")
+
+    @property
+    def name(self) -> str:
+        return self._data.get("name")
+
+    @property
+    def visibility(self) -> str:
+        return self._data.get("visibility")
+
+    @property
+    def owner(self) -> dict:
+        return self._data.get("owner")
+
+    @property
+    def supported_clusters(self) -> list:
+        return self._data.get("supportedClusters")
+
+
+class EnvironmentManager:
+    _default_environment: Environment
+    _default_environment_details: dict
+
+    def __init__(self, base_url: str, api_key=None, domino_token_file=None):
         self._configure_logging()
 
         base_url: str = clean_host_url(get_host_or_throw_exception(base_url))
@@ -73,7 +120,7 @@ class Environment:
 
         # Get Domino deployment version
         self._version = self.deployment_version()
-        self._logger.info(f"Domino deployment {base_url} is running version {self._version}")
+        self.log.info(f"Domino deployment {base_url} is running version {self._version}")
 
         # Check version compatibility
         if not is_version_compatible(self._version):
@@ -81,10 +128,10 @@ class Environment:
                 f"Domino version: {self._version} is not compatible with "
                 f"python-domino_environments-environments version: {__version__}"
             )
-            self._logger.error(error_message)
+            self.log.error(error_message)
             raise Exception(error_message)
 
-        self.refresh(environment_id)
+        self.refresh_defaults()
 
     @property
     def log(self):
@@ -106,56 +153,97 @@ class Environment:
                 "must be provided via class constructor or environment variable"
             )
         elif domino_token_file is not None:
-            self._logger.info(
-                "Initializing python-domino_environments-environments with bearer token auth")
+            self.log.info(
+                "Initializing python-domino_environments-environments with bearer token auth"
+            )
             return _HttpRequestManager(BearerAuth(domino_token_file))
         else:
-            self._logger.info(
-                "Fallback: Initializing python-domino_environments-environments with basic auth")
+            self.log.info(
+                "Fallback: Initializing python-domino_environments-environments with basic auth"
+            )
             return _HttpRequestManager(HTTPBasicAuth("", api_key))
 
     def deployment_version(self):
         url = self._routes.deployment_version()
         return self.request_manager.get(url).json().get("version")
 
-    def refresh(self, environment_id: str = None, revision_id: str = None):
-        self._env_data = self.get_environment(environment_id)
-        self._env_details = self.get_revision_details(environment_id, revision_id)
+    def refresh_defaults(self):
+        self._default_environment = self.get_default_environment()
+        self._default_environment_details = self.get_revision_details(self._default_environment)
 
-        self._default_env_data = self.get_default_environment()
-        self._default_env_details = self.get_revision_details(
-            self._default_env_data["id"],
-            self._default_env_data["selectedRevision"]["id"],
-        )
+    def get_default_environment(self) -> Environment:
+        url = self._routes.environment_default_get()
+        data = self.request_manager.get(url).json()
+        return Environment(data)
 
-    def archive_environment(self):
-        url = self._routes.environment_remove(self._id)
+    def archive_environment(self, environment: Environment):
+        url = self._routes.environment_remove(environment.id)
         return self.request_manager.post(url)
 
-    def get_default_environment(self):
-        url = self._routes.environment_default_get()
-        return self.request_manager.get(url).json()
-
-    def get_environment(self, environment_id: str = None):
-        environment_id = environment_id or self._id
+    def get_environment(self, environment_id: str) -> Environment:
         url = self._routes.environment_get(environment_id)
-        return self.request_manager.get(url).json()
+        data = self.request_manager.get(url).json()
+        return Environment(data)
 
-    def get_revision_details(self, environment_id: str = None, revision_id: str = None) -> dict:
+    def create_environment(
+        self,
+        name: str,
+        image_type: str,
+        visibility: str,
+        description: str = "",
+        docker_image: str = "",
+        base_environment_revision_id: str = None,
+        base_default_environment_image: str = None,
+        user_owner_id: str = None,
+        organization_owner_id: str = None,
+        cluster_types: str = None,
+    ):
+        if not base_environment_revision_id:
+            base_environment_revision_id = self._default_environment.active_revision["id"]
+
+        if not base_default_environment_image:
+            base_default_environment_image = self._default_environment_details["Dockerfile"][
+                "base_image"
+            ]
+
+        form_payload = {
+            "name": name,
+            "description": description,
+            "visibility": visibility,
+            "base.imageType": image_type,
+            "base.dockerImage": docker_image,
+            "base.baseEnvironmentRevisionId": base_environment_revision_id,
+            "base.defaultEnvironmentImage": base_default_environment_image,
+        }
+
+        if visibility == Visibility.organization:
+            form_payload["organizationOwnerId"] = organization_owner_id
+
+        if user_owner_id:
+            form_payload["userOwnerId"] = user_owner_id
+
+        if cluster_types:
+            form_payload["clusterTypes[]"] = cluster_types
+
+        return self.request_manager.post(
+            url=self._routes.environment_create(),
+            data=form_payload,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+    def get_revision_details(self, environment: Environment, revision_id: str = None) -> dict:
         """Gather Dockerfile instructions, Pre/Post Setup Script, and Pre/Post Run Script info.
 
         Args:
-            environment_id: The ID of the environment, defaults to self ID.
-            revision_id: The ID of the environment's revision, defaults to self active revision ID.
-            revision_id: The ID of the environment's revision, defaults to self active revision ID.
+            environment: The environment object.
+            revision_id: The ID of the revision, defaults to the environment's active revision.
         """
-        environment_id = environment_id or self._id
-        revision_id = revision_id or self.active_revision.get("id")
-        scraped_data = self._scrape_revision(environment_id, revision_id)
-        return scraped_data
+        revision_id = revision_id or environment.active_revision.get("id")
+        return self._scrape_revision(environment.id, revision_id)
 
     def create_revision(
         self,
+        environment: Environment,
         image_type: str,
         docker_image: str = "",
         base_environment_revision_id: str = None,
@@ -174,10 +262,12 @@ class Environment:
         summary: str = "",
     ):
         if not base_environment_revision_id:
-            base_environment_revision_id = self._default_env_data["selectedRevision"]["id"]
+            base_environment_revision_id = self._default_environment.active_revision["id"]
 
         if not base_default_environment_image:
-            base_default_environment_image = self._default_env_details["Dockerfile"]["base_image"]
+            base_default_environment_image = self._default_environment_details["Dockerfile"][
+                "base_image"
+            ]
 
         form_payload = {
             "base.imageType": image_type,
@@ -208,47 +298,14 @@ class Environment:
         if cluster_types:
             form_payload["clusterTypes[]"] = cluster_types
 
-        url = self._routes.revision_create(self._id)
         return self.request_manager.post(
-            url=url,
+            url=self._routes.revision_create(environment.id),
             data=form_payload,
             headers={"Content-Type": "application/x-www-form-urlencoded"},
         )
 
-    def _scrape_revision(self, environment_id, revision_id):
+    def _scrape_revision(self, environment_id: str, revision_id: str) -> dict:
         url = self._routes.revision_download(environment_id, revision_id)
         res = self.request_manager.get(url)
         file_io = io.BytesIO(res.content)
         return parse_revision_tar(file_io)
-
-    @property
-    def _id(self) -> str:
-        return self._env_data.get("id")
-
-    @property
-    def latest_revision(self) -> dict:
-        return self._env_data.get("latestRevision")
-
-    @property
-    def active_revision(self) -> dict:
-        return self._env_data.get("selectedRevision")
-
-    @property
-    def archived(self) -> bool:
-        return self._env_data.get("archived")
-
-    @property
-    def name(self) -> str:
-        return self._env_data.get("name")
-
-    @property
-    def visibility(self) -> str:
-        return self._env_data.get("visibility")
-
-    @property
-    def owner(self) -> dict:
-        return self._env_data.get("owner")
-
-    @property
-    def supported_clusters(self) -> list:
-        return self._env_data.get("supportedClusters")
