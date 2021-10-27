f3a431682be8480abebf1055580defead384d642
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2021-10-27 14:46:07 +0000

    Updated python-domino package requirements bump version

--- domino_environments/__init__.py
+++ domino_environments/__init__.py
@@ -2,9 +2,9 @@ from .utils import parse_version
 
 from domino import __version__
 
-min_version = "1.0.4"
+min_version = "1.0.5"
 if parse_version(__version__) < parse_version(min_version):
-    raise ImportError(f"python-domino>=1.0.4 is required, {__version__} is installed")
+    raise ImportError(f"python-domino>={min_version} is required, {__version__} is installed")
 
 from ._environments import Environment, EnvironmentManager, ImageType, ClusterType, Visibility
 
