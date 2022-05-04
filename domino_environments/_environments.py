3dc925173f212c68af864eb392b9695d2f70c3d8
Dranaxel <a.marce@free.fr>
2022-05-04 19:08:34 +0200

    typo

--- domino_environments/_environments.py
+++ domino_environments/_environments.py
@@ -205,7 +205,7 @@ class EnvironmentManager:
         self._default_details = self.get_revision_details(self._default_environment)
 
     def get_environments_list(self):
-        url = self._routes.get_environments()
+        url = self._routes.get_environments_list()
         return self.request_manager.get(url).json().get('data')
 
     def get_default_environment(self) -> Environment:
